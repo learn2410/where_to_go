1805d010ce3e0037a92b6cce0382b757c95a8e9e
learn2410 <learn2410@mail.ru>
2022-01-19 00:10:15 +0500

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,51 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, Image
 
+
+# class ImageInline(admin.TabularInline):
+#     model = Image
+#     fk_name = "placeid"
+#     # list_display = ('img_preview' )
+#     fields=['img_preview',('number','img')]
+#     readonly_fields = ('img_preview',)
+#
+#     def img_preview(self, obj):
+#         return obj.img_preview
+#
+#     img_preview.short_description = 'Preview'
+#     img_preview.allow_tags = True
+#
+#     class Meta:
+#         ordering = ['number']
+#         verbose_name = 'фото'
+#         verbose_name_plural = 'фотографии'
+#
 
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('title',)
     ordering = ['title']
+    # inlines = [
+    #     ImageInline,
+    # ]
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('place_num','img_preview')
+    list_filter = ['placeid']
+    readonly_fields = ('img_preview',)
+    ordering = ('placeid','number')
+
+    def img_preview(self, obj):
+        return obj.img_preview
+
+    img_preview.short_description = 'Preview'
+    img_preview.allow_tags = True
+
+    class Meta:
+        ordering = ['placeid','number']
+        verbose_name = 'фото'
+        verbose_name_plural = 'фотографии'
 
 
 admin.site.register(Place, PlaceAdmin)
+admin.site.register(Image,ImageAdmin)
--- places/models.py
+++ places/models.py
@@ -1,9 +1,29 @@
 from django.db import models
+from django.conf import settings
+from django.utils.html import mark_safe
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=100, null=False, blank=False, verbose_name="заголовок")
-    description_short = models.CharField(max_length=256, null=False, blank=True, verbose_name="короткое описание")
-    description_long = models.TextField(null=False, blank=True, verbose_name="длинное описание")
+    title = models.CharField(max_length=100, null=False, blank=True)
+    description_short = models.CharField(max_length=256, null=False, blank=True)
+    description_long = models.TextField(null=False, blank=True)
     lng = models.FloatField(verbose_name="долгота")
     lat = models.FloatField(verbose_name="широта")
+
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    placeid=models.ForeignKey(Place,on_delete=models.CASCADE,null=False)
+    number=models.IntegerField("номер",default=0)
+    img=models.ImageField(upload_to='image')
+
+    @property
+    def place_num(self):
+        return '{} - {}'.format(self.number,self.placeid)
+
+    @property
+    def img_preview(self):
+        if self.img:
+            return mark_safe('<img src="{}" height="140" />'.format(self.img.url))
+        return ""
