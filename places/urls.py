a78d9f691acb55fa5cce082e4e26b41d07bc9b2a
learn2410 <learn2410@mail.ru>
2022-01-30 12:32:26 +0500

    Move data from template to view

--- places/urls.py
+++ places/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+app_name = 'places'
 urlpatterns = [
     path('', views.view_blank),
+    path('jsn/place_<int:idfromurl>',views.detail_json,name='json_place')
 ]
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', view_tmp),
-    path('places/', include('places.urls'))
+    path('old', view_tmp),
+    path('', include('places.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
