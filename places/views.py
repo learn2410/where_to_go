38ddbe1f379128258d2cf7df17351cfd7895088d
learn2410 <learn2410@mail.ru>
2022-05-05 23:12:22 +0500

    HTTPResponse change to JsonResponse and optimized code

--- places/views.py
+++ places/views.py
@@ -1,7 +1,6 @@
-import json
-
 from django.conf import settings
-from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Place
 
@@ -21,22 +20,6 @@ def geojson_feature(place):
     }
 
 
-def get_place_json(place):
-    image_urls = [f'{settings.MEDIA_URL}{s}'
-                  for s in place.images.order_by('number').values_list('img', flat=True)]
-
-    payload_place = {"title": place.title,
-                     "imgs": image_urls,
-                     "description_short": place.description_short,
-                     "description_long": place.description_long,
-                     "coordinates": {
-                         "lng": place.lng,
-                         "lat": place.lat
-                     }
-                     }
-    return json.dumps(payload_place, ensure_ascii=False)
-
-
 def make_geodict(dataset):
     return {"type": "FeatureCollection", "features": [geojson_feature(row) for row in dataset]}
 
@@ -49,5 +32,16 @@ def view_blank(request):
 
 def detail_json(request, place_id):
     place = get_object_or_404(Place, pk=place_id)
-    content = get_place_json(place)
-    return HttpResponse(content, content_type='application/json')
+    image_urls = [f'{settings.MEDIA_URL}{s}'
+                  for s in place.images.order_by('number').values_list('img', flat=True)]
+    payload_place = {
+        "title": place.title,
+        "imgs": image_urls,
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.lng,
+            "lat": place.lat
+        }
+    }
+    return JsonResponse(payload_place, json_dumps_params={'indent': 2, 'ensure_ascii': False})
